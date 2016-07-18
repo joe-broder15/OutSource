1ea3a2109c615056e89fba9b31dc85e7b97d6691
joe-broder15 <joey.broder@gmail.com>
2016-07-18 16:09:59 -0700

    Appends cells to the interest table

--- OutSource/ViewControllers/InterestsVC.swift
+++ OutSource/ViewControllers/InterestsVC.swift
@@ -13,32 +13,50 @@ import Firebase
 class InterestsVC: UITableViewController {
 
     let firebaseHelper = FirebaseHelper()
+    var interests: [FIRDataSnapshot]! = []
+    private var _refHandle: FIRDatabaseHandle!
+    var ref: FIRDatabaseReference!
     
+    
+        
     override func viewDidLoad() {
+        self.configureDatabase()
         super.viewDidLoad()
-       // firebaseHelper.getInterests()
+        
+    }
+    
+    func configureDatabase() {
+        ref = FIRDatabase.database().reference()
+        // Listen for new messages in the Firebase database
+        _refHandle = self.ref.child("Interests").observeEventType(.ChildAdded, withBlock: { (snapshot) -> Void in
+            self.interests.append(snapshot)
+            //print(self.interests)
+            //print(self.interests[0].value!)
+            self.tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: self.interests.count-1, inSection: 0)], withRowAnimation: .Automatic)
+        })
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 9
+        return self.interests.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("tableCell", forIndexPath: indexPath) as! TableViewCell
         
-//        //1
-//        let row = indexPath.row
-//        
-//        //2
-//        let note = notes[row]
-//        
-//        //3
-//        cell.noteTitleLabel.text = note.title
-//        
-//        //4
-//        cell.noteTimeModifiedLabel.text = note.modificationTime.convertToString()
+        //1
+        let row = indexPath.row
         
+        //2
+        let interest = interests[row].value
+        
+        //3
+        cell.textLabel!.text = interest! as? String
+
+        //4
+        //cell.noteTimeModifiedLabel.text = note.modificationTime.convertToString()
+
         return cell
+        
     }
     
     
